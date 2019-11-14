b98c4a6b6da7bc6e1d5e9898285b0d0670c8e95b
thayna-oliveira <oliveira.thayna@outlook.com>
2019-11-14 16:38:38 -0300

    add style

--- src/App.js
+++ src/App.js
@@ -8,30 +8,27 @@ function App() {
   return (
     <div>
 
-        <div className="nav">
-          <div className="container">
-            <a href="https://superlogica.com" target="_blank">
-              <img src="https://recorrencia.superlogica.com/hubfs/%5BGLOBAL%20ASSETS%5D/cards/2927_LP_tarifa_fixa/brand/superlogica.svg" alt="Logo Superlógica" class="logo" />
-            </a>
-          </div>
+      <div className="nav">
+        <div className="container">
+          <a href="https://superlogica.com" target="_blank">
+            <img src="https://recorrencia.superlogica.com/hubfs/%5BGLOBAL%20ASSETS%5D/cards/2927_LP_tarifa_fixa/brand/superlogica.svg" alt="Logo Superlógica" class="logo" />
+          </a>
         </div>
-
-
-        <div className="hero teste">
-          <div className="container">
-            <Gerador />
-          </div>
-        </div>
-
-
-        <div className="footer">
-          <div className="container">
-            <p className="text-center text-white m-0">© 2019 Superlógica Tecnologias</p>
-          </div>
-        </div>
-
-
       </div>
+
+      <div className="hero">
+        <div className="container">
+          <Gerador />
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="container">
+          <p className="text-center text-white m-0">© 2019 Superlógica Tecnologias</p>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import './assets/css/bootstrap-grid.min.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/main.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
